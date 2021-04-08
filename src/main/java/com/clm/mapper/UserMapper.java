161db4b9378b274c405dfe7cbb850e8b2c92a88d
XDEcat <492697494@qq.com>
2021-04-08 13:24:08 +0800

    添加了UserMapper

--- src/main/java/com/clm/mapper/UserMapper.java
+++ src/main/java/com/clm/mapper/UserMapper.java
@@ -1,11 +1,18 @@
 package com.clm.mapper;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * date: 2021年04月08日 11:18
  * 描述:
  * Author: 薛定谔的猫er
  */
+@Repository
 public interface UserMapper {
     // 增加用户
     boolean insertUser();
+    // 删除用户
+    boolean deleteUser();
+    // 查询用户
+    boolean findUser();
 }
